c96729a1adda633606f7a6655df24668ad62bce7
Aleksandr Kuznetsov <kuznetzov.kuznetsov2004@yandex.ru>
2025-04-11 21:35:13 +0300

    первая попытка

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
-
-// @todo: DOM узлы
+//получение нужных эл-тов
+const cardList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCard) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const card = cardElement.querySelector(".places__item");
+
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardImage.src = cardData.link;
+  cardTitle.textContent = cardData.name;
+
+  cardList.append(card);
+
+  deleteButton.addEventListener("click", () => {
+    deleteCard(card);
+  });
+  return card;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(item) {
+  item.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((element) => {
+  createCard(element, deleteCard);
+});
